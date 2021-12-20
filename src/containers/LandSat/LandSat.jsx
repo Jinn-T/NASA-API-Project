1f06284f3eb7ca2fb9f9ea5dc860b801e19665b5
Jinn Tsang <jinntsang@192-168-1-101.tpgi.com.au>
2021-12-20 22:07:59 +1100

    figuring out response for landsat api

--- src/containers/LandSat/LandSat.jsx
+++ src/containers/LandSat/LandSat.jsx
@@ -3,25 +3,36 @@ import { useEffect, useState } from "react";
 
 const LandSat = () => {
     // api call for landsat imagery
-    const [landSatImagery, setLandSatImagery] = useState("null");
+    const [landSatImagery, setLandSatImagery] = useState(null);
 
     useEffect(() => {
         const getLandSatImagery = async () => {
+            // const response = await fetch(
+            //     `https://api.nasa.gov/planetary/earth/imagery?lon=30.099033&lat=51.389636&api_key=xtvhA8LjsnJ0AZ1bbpP4cWQJ6HjB7UEmhuR8lnjf`
+            // );
             const response = await fetch(
-                `https://api.nasa.gov/planetary/earth/imagery?lon=30.099033&lat=51.389636&api_key=xtvhA8LjsnJ0AZ1bbpP4cWQJ6HjB7UEmhuR8lnjf`
+                `https://api.nasa.gov/planetary/earth/imagery?lon=30.099033&lat=51.389636&date=2014-02-01&api_key=DEMO_KEY`
             );
-            const data = response.json();
-            console.log(data);
-            setLandSatImagery(data);
+            // console.log("response:", response);
+            // const data = await response.json();
+            // console.log("data:", data);
+            setLandSatImagery(response);
         };
-        // getLandSatImagery();
+        getLandSatImagery();
     }, []);
 
-    console.log(landSatImagery);
+    console.log("landsatimagery state:", landSatImagery);
 
     return (
         <div>
             <h1>hello</h1>
+            {landSatImagery && (
+                <img
+                    src={landSatImagery.url}
+                    alt="test"
+                    className={styles.landSatPic}
+                />
+            )}
         </div>
     );
 };
