965360841a7735e2f6a5eba3f53805efb462446d
Jinn Tsang <jinntsang@192-168-1-101.tpgi.com.au>
2022-01-06 16:25:13 +1100

    added longitude search functionality

--- src/containers/LandSat/LandSat.jsx
+++ src/containers/LandSat/LandSat.jsx
@@ -1,4 +1,5 @@
 import styles from "./LandSat.module.scss";
+import LandSatSearch from "../../components/LandSatSearch";
 import { useEffect, useState } from "react";
 const key = "xtvhA8LjsnJ0AZ1bbpP4cWQJ6HjB7UEmhuR8lnjf";
 const demo = `DEMO_KEY`;
@@ -10,10 +11,13 @@ const LandSat = () => {
     // lat = N and S
     // lon = E and W
 
+    // longitude state
+    const [longSearch, setLongSearch] = useState(-110.8358417);
+
     useEffect(() => {
         const getLandSatImagery = async () => {
             const response = await fetch(
-                `https://api.nasa.gov/planetary/earth/assets?lon=-110.8358417&lat=32.1499889&date=2020-11-01&&dim=0.155&api_key=${key}`
+                `https://api.nasa.gov/planetary/earth/assets?lon=${longSearch}&lat=32.1499889&date=2020-11-01&&dim=0.155&api_key=${key}`
             );
             console.log("response:", response);
             const data = await response.json();
@@ -21,13 +25,21 @@ const LandSat = () => {
             setLandSatImagery(data);
         };
         getLandSatImagery();
-    }, []);
+    }, [longSearch]); // watch the search states
 
     console.log("landsatimagery state:", landSatImagery);
+
+    // function handler which we will pass down to our search component, allowing us to update the state.
+    const handleSubmit = (longValue) => {
+        setLongSearch(longValue);
+    };
+
+    console.log("longSearch:", longSearch);
 
     return (
         <div>
             <h1>hello</h1>
+            <LandSatSearch onSubmit={handleSubmit} />
             {landSatImagery && (
                 <img
                     src={landSatImagery.url}
