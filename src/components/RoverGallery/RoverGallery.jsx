fe2ff12c3f14170dc33d647ab28705e0d7eec232
Your Name <jinntsang@gmail.com>
2022-02-08 16:02:55 +1100

    displaying mars rover picture from nasa api object

--- src/components/RoverGallery/RoverGallery.jsx
+++ src/components/RoverGallery/RoverGallery.jsx
@@ -1,11 +1,8 @@
 import styles from "./RoverGallery.module.scss";
 
-const RoverGallery = () => {
-  return (
-    <div>
-      <p>higrgrege</p>
-    </div>
-  );
+const RoverGallery = ({ roverPic }) => {
+    console.log("roverPic Prop:", roverPic);
+    return <div>{<img src={roverPic.img_src} alt="" />}</div>;
 };
 
 export default RoverGallery;
