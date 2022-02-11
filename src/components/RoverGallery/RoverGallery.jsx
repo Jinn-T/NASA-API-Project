3338e5610183a9403d2218c6baaacf1ab1579b53
Your Name <jinntsang@gmail.com>
2022-02-11 16:52:33 +1100

    now able to render pictures from the Mars rover object, added bootstrap radio buttons which will alternate between each rover camera. Still need to add some logic to update button state.

--- src/components/RoverGallery/RoverGallery.jsx
+++ src/components/RoverGallery/RoverGallery.jsx
@@ -1,8 +1,19 @@
 import styles from "./RoverGallery.module.scss";
+import { Card, Button } from "react-bootstrap";
 
 const RoverGallery = ({ roverPic }) => {
     console.log("roverPic Prop:", roverPic);
-    return <div>{<img src={roverPic.img_src} alt="" />}</div>;
+    return (
+        <div>
+            {/* {<img src={roverPic.img_src} alt="" />} */}
+            <Card style={{ width: "36rem" }}>
+                <Card.Img variant="top" src={roverPic.img_src} />
+                {/* <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                </Card.Body> */}
+            </Card>
+        </div>
+    );
 };
 
 export default RoverGallery;
--- src/containers/MarsRover/MarsRover.jsx
+++ src/containers/MarsRover/MarsRover.jsx
@@ -1,6 +1,7 @@
 import styles from "./MarsRover.module.scss";
 import RoverGallery from "../../components/RoverGallery";
 import { useEffect, useState } from "react";
+import { ButtonGroup, Button } from "react-bootstrap";
 const key = "xtvhA8LjsnJ0AZ1bbpP4cWQJ6HjB7UEmhuR8lnjf";
 const demo = `DEMO_KEY`;
 
@@ -21,20 +22,64 @@ const MarsRover = () => {
 
     console.log("roverPhotos:", roverPhotos);
     //   console.log("test:", JSO.sol_keys);
-    //test
+
+    // console.log("roverPhotos.photos", roverPhotos.photos);
+
+    // test cleaned data
+    // const cleanedRoverPhotos = (data) => {
+    //     let newArr = [];
+    //     for (let i = 0; i < data.photos.length; i++) {
+    //         if (newArr.includes(data.photos[i].id)) {
+    //             console.log("duplicate");
+    //         } else {
+    //             newArr.push(data.photos[i].id);
+    //         }
+    //     }
+    //     console.log(newArr);
+    //     return newArr;
+    // };
+
+    // if (roverPhotos !== null) {
+    //     console.log("cleanedData:", cleanedRoverPhotos(roverPhotos));
+    // }
+
+    // onclick - changes state and allows us to toggle different cameras
+
+    const [roverCamera, setRoverCamera] = useState("MAST");
+
+    const handleClick = (camera) => {
+        setRoverCamera(camera.target.value);
+    };
+    console.log("roverCamera State:", roverCamera);
 
     return (
         <div>
-            <h3>
+            <h3 className={styles.desc}>
                 Photos taken on Curiosity's 1000th Martian sol exploration of
                 Mars.
             </h3>
-            {roverPhotos &&
-                roverPhotos.photos
-                    .filter((pics, index) => index < 12)
-                    .map((pic, index) => (
-                        <RoverGallery roverPic={pic} key={index} />
-                    ))}
+            <div className={styles.buttonGroup}>
+                <ButtonGroup aria-label="Basic example">
+                    <Button variant="secondary" onClick={handleClick}>
+                        FHAZ
+                    </Button>
+                    <Button variant="secondary">RHAZ</Button>
+                    <Button variant="secondary">MAST</Button>
+                    <Button variant="secondary">CHEMCAM</Button>
+                    <Button variant="secondary">MAHLI</Button>
+                    <Button variant="secondary">MARDI</Button>
+                    <Button variant="secondary">NAVCAM</Button>
+                </ButtonGroup>
+            </div>
+
+            <div className={styles.gallery}>
+                {roverPhotos &&
+                    roverPhotos.photos
+                        .filter((pics, index) => index < 12)
+                        .map((pic, index) => (
+                            <RoverGallery roverPic={pic} key={index} />
+                        ))}
+            </div>
         </div>
     );
 };
