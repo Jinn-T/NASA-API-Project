6c77dc09f489391a2a078d491e24504e6b73bee7
Your Name <jinntsang@gmail.com>
2022-02-14 12:55:54 +1100

    styling cards and adding additional information for users

--- src/components/RoverGallery/RoverGallery.jsx
+++ src/components/RoverGallery/RoverGallery.jsx
@@ -8,9 +8,15 @@ const RoverGallery = ({ roverPic }) => {
             {/* {<img src={roverPic.img_src} alt="" />} */}
             <Card style={{ width: "36rem" }}>
                 <Card.Img variant="top" src={roverPic.img_src} />
-                {/* <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                </Card.Body> */}
+                <Card.Body className={styles.cardBody}>
+                    {/* <Card.Title>Card Title</Card.Title> */}
+                    <Card.Text>Rover Name: {roverPic.rover.name}</Card.Text>
+                    <Card.Text>Status: {roverPic.rover.status}</Card.Text>
+                    <Card.Text>Earth Date: {roverPic.earth_date}</Card.Text>
+                    <Card.Text>
+                        Landing Date: {roverPic.rover.landing_date}
+                    </Card.Text>
+                </Card.Body>
             </Card>
         </div>
     );
