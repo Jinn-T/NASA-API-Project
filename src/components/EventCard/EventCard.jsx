5033da78bd5e5c6323b25ecc5403b72ffbf20784
Your Name <jinntsang@gmail.com>
2022-02-21 15:08:53 +1100

    created event card component and now looking to render the data to the page.

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,6 +1,7 @@
 import styles from "./EventCard.module.scss";
 
-const EventCard = () => {
+const EventCard = ({ event }) => {
+    console.log("event inside component", event);
     return <div></div>;
 };
 export default EventCard;
